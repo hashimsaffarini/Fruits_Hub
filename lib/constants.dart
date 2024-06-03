3edbc51175d0ab642b4c069a1b9884cabc8db7ba
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-06-03 16:11:58 +0300

    feat: Add horizontal padding constant and update CustomButton style

---
+++ lib/constants.dart
@@ -0,0 +1,1 @@
+const kHorizantalPadding = 16.0;
--- lib/features/on_boarding/ui/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/ui/widgets/on_boarding_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/constants.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
+import 'package:fruits_hub/core/widgets/custom_button.dart';
 import 'package:fruits_hub/features/on_boarding/ui/widgets/on_boarding_page_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -20,6 +22,15 @@ class OnBoardingViewBody extends StatelessWidget {
             activeColor: AppColors.primaryColor,
           ),
         ),
+        const SizedBox(height: 29),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kHorizantalPadding),
+          child: CustomButton(
+            onPressed: () {},
+            text: 'ابدأ الان',
+          ),
+        ),
+        const SizedBox(height: 43),
       ],
     );
   }
