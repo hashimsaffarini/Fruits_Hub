dcdfc6b14831e7f7ff29ed02386f7acbbdd157b6
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-06-03 18:55:12 +0300

    chore: Update font family and add Cairo font weights

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,9 @@ class FruitHub extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Cairo',
+      ),
       localizationsDelegates: const [
         S.delegate,
         GlobalMaterialLocalizations.delegate,
